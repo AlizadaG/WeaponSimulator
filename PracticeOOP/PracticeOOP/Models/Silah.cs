26a76af08cfb65e2a1fd6b3fcc7c35ee8a7ab0eb
AlizadaG <elizadegozel@gmail.com>
2024-07-10 12:35:51 +0400

    Weapon simultor

---
+++ PracticeOOP/PracticeOOP/Models/Silah.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeOOP.Models
+{
+    public class Silah
+    {
+        public string Nov;
+        private int _tutum;
+        public int Tutum
+        {
+            get
+            {
+                return _tutum;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Tutum menfi ola bilmez...");
+                }
+                _tutum = value;
+            }
+        }
+        private int _cariGulleSayi;
+        public int CariGulleSayi
+        {
+            get {
+                return _cariGulleSayi;
+            }
+            set
+            {
+                if(value < 0) {
+                    Console.WriteLine("Cari gulle sayi menfi ola bilmez");
+                }
+                _cariGulleSayi = value;
+            }
+        }
+        private int _saniyedeGulleSayi;
+        public int SaniyedeGulleSayi
+        {
+            get
+            {
+                return _saniyedeGulleSayi;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Saniyede atilan gulle sayi menfi ola bilmez");
+                }
+                _saniyedeGulleSayi = value;
+            }
+        }
+
+        public void TekAtis(int gulleSayi)
+        {
+            for (int i = 0; i < gulleSayi; i++)
+            {
+                if (_cariGulleSayi > 0)
+                {
+                    CariGulleSayi--;
+                    Console.WriteLine("Gulle atildi ve maqazinde " + _cariGulleSayi + " sayda gulle qaldi...");
+                }
+                else
+                {
+                    Console.WriteLine("Hal hazirda maqazinde gulle yoxdur, doldurmaq üçün R duymesine klikleyin...");
+                    string btn = Console.ReadLine().ToLower();
+                    if (btn == "r")
+                    {
+                        _cariGulleSayi = Tutum;
+                        Console.WriteLine("Gulle dolduruldu ve hazirda maqazinde " + CariGulleSayi + " sayda gulle var...");
+                    }
+                    break;
+                }
+            }
+        }
+       
+        public void AvtomatAtis(int time)
+        {
+            int gulleSayi = time * SaniyedeGulleSayi;
+            TekAtis(gulleSayi);
+        }
+    }
+}
---
+++ PracticeOOP/PracticeOOP/Program.cs
@@ -0,0 +1,56 @@
+﻿using PracticeOOP.Models;
+
+namespace PracticeOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Silah silah = new Silah();
+            silah.Nov = "ShotGun";
+            silah.Tutum = 40;
+            silah.CariGulleSayi = 40;
+            silah.SaniyedeGulleSayi = 5;
+
+
+            while (true) {
+                Console.WriteLine($"Oyuna xos geldiniz. \n Tek-tek atis etmek ucun A, \n Avtomatik atislar ucun D ,\n Cixis etmek ucun ise W duymesine klikleyin...");
+                string btn = Console.ReadLine().ToLower();
+                if (btn == "a")
+                {
+                    Console.Write("Atis miqdarini daxil edin:");
+                    try
+                    {
+                        int count = Convert.ToInt32(Console.ReadLine());
+                        silah.TekAtis(count);
+                    }
+                    catch (Exception message)
+                    {
+                        Console.WriteLine("Sayı doğru formatda dahil edin. Xeta mesajı:" + message);
+                    }
+                }
+                else if (btn == "d")
+                {
+                    Console.Write("Nece saniye atis olacagini daxil edin:");
+                    try
+                    {
+                        int count = Convert.ToInt32(Console.ReadLine());
+                        silah.AvtomatAtis(count);
+                    }
+                    catch (Exception message)
+                    {
+                        Console.WriteLine("Sayı doğru formatda dahil edin. Xeta mesajı:" + message);
+                    }
+                }
+                else if (btn == "w") {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Daxil etdiyiniz degerler oyun ucun kecerli deyil...");
+                }
+            }
+
+        }
+    }
+}
